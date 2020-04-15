df2eee772dc19d432d66e18651d1d20d909ad958
aknuck <adamknuckey@gmail.com>
2020-04-15 18:59:53 -0400

    added Coinbase and Zerion partners

--- src/components/Partners/Partners.js
+++ src/components/Partners/Partners.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HuobiLogo from '../../assets/Huobi-logo.png';
 import ChainlinkLogo from '../../assets/Chainlink-logo.png';
 import DraperVentureNetwork from '../../assets/Draper-Venture-Network-logo.png';
+import CoinbaseLogo from '../../assets/Coinbase-logo.png';
 import FortmaticLogo from '../../assets/Fortmatic-logo.png';
 import TrustWalletLogo from '../../assets/TrustWallet-Logo.png';
 import DIFCLogo from '../../assets/DIFC-logo.png';
@@ -9,6 +10,7 @@ import GnosisLogo from '../../assets/Gnosis-logo.png';
 import TotleLogo from '../../assets/Totle-logo.png';
 import BlocknativeLogo from '../../assets/Blocknative-logo.png';
 import LoopringLogo from '../../assets/Loopring-logo.png';
+import ZerionLogo from '../../assets/Zerion-logo.png';
 
 import './Partners.css';
 
@@ -28,52 +30,62 @@ class Partners extends React.Component {
             <div className={'partnersWrapper'}>
               <div className={'publication'}>
                 <div className={'partnersInner'}>
-                  <img src={HuobiLogo}/>
+                  <a href={'https://www.huobi.com/'} target="_blank"><img src={HuobiLogo}/></a>
                 </div>
               </div>
               <div className={'publication'}>
                 <div className={'partnersInner'}>
-                  <img src={ChainlinkLogo}/>
+                  <a href={'https://chain.link/'} target="_blank"><img src={ChainlinkLogo}/></a>
                 </div>
               </div>
               <div className={'publication'}>
                 <div className={'partnersInner'}>
-                  <img src={DraperVentureNetwork}/>
+                  <a href={'https://drapernetwork.com/'} target="_blank"><img src={DraperVentureNetwork}/></a>
                 </div>
               </div>
               <div className={'publication'}>
                 <div className={'partnersInner'}>
-                  <img src={FortmaticLogo}/>
+                  <a href={'https://www.coinbase.com/'} target="_blank"><img src={CoinbaseLogo}/></a>
                 </div>
               </div>
               <div className={'publication'}>
                 <div className={'partnersInner'}>
-                  <img src={TrustWalletLogo}/>
+                  <a href={'https://fortmatic.com/'} target="_blank"><img src={FortmaticLogo}/></a>
                 </div>
               </div>
               <div className={'publication'}>
                 <div className={'partnersInner'}>
-                  <img src={DIFCLogo}/>
+                  <a href={'https://trustwallet.com/'} target="_blank"><img src={TrustWalletLogo}/></a>
                 </div>
               </div>
               <div className={'publication'}>
                 <div className={'partnersInner'}>
-                  <img src={GnosisLogo}/>
+                  <a href={'https://www.difc.ae/'} target="_blank"><img src={DIFCLogo}/></a>
                 </div>
               </div>
               <div className={'publication'}>
                 <div className={'partnersInner'}>
-                  <img src={TotleLogo}/>
+                  <a href={'https://gnosis.io/'} target="_blank"><img src={GnosisLogo}/></a>
                 </div>
               </div>
               <div className={'publication'}>
                 <div className={'partnersInner'}>
-                  <img src={BlocknativeLogo}/>
+                  <a href={'https://www.totle.com/'} target="_blank"><img src={TotleLogo}/></a>
                 </div>
               </div>
               <div className={'publication'}>
                 <div className={'partnersInner'}>
-                  <img src={LoopringLogo}/>
+                  <a href={'https://www.blocknative.com/'} target="_blank"><img src={BlocknativeLogo}/></a>
+                </div>
+              </div>
+              <div className={'publication'}>
+                <div className={'partnersInner'}>
+                  <a href={'https://loopring.org/#/'} target="_blank"><img src={LoopringLogo}/></a>
+                </div>
+              </div>
+              <div className={'publication'}>
+                <div className={'partnersInner'}>
+                  <a href={'https://zerion.io/'} target="_blank"><img src={ZerionLogo}/></a>
                 </div>
               </div>
             </div>
